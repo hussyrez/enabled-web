56a36d5e396fdc83d949408ae0cc740b114bb4f2
Heath Neville <heath@enabled.com.au>
2015-03-18 12:27:28 +1030

    Change content slider to scroll left/right instead of fade

--- website/js/enabled_scripts.js
+++ website/js/enabled_scripts.js
@@ -78,14 +78,22 @@ function jumpScroll() {
   $('.content-slider').slick({
     dots: true,
     infinite: true,
-    speed:1000,
+    speed:180,
     slidesToShow: 1,
-    fade: true,
+    //fade: true,
     autoplay: false,
     arrows: true,
     autoplaySpeed: 1000,
     adaptiveHeight: false
-  });			
+  });
+
+$('.content-slider').on('beforeChange', function(event, slick, currentSlide, nextSlide){
+	//classie.add($('.content-slider').get(currentSlide), 'fade');
+	//classie.remove($('.content-slider').get(nextSlide), 'fade');
+	//$(this).find(".slide-content:nth-child("+(currentSlide+1)+")").fadeOut();
+	//$(this).find(".slide-content:nth-child("+(nextSlide+1)+")").fadeIn();
+	console.log("Slide: " + currentSlide + ", " + nextSlide);
+});
 
 //function slideIsPastTop(slideName)
 //{
