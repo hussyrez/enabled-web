3ff57a3cf3cb02ca3a3a65bb74f1afdf11306cd1
Nola Falk <nola@enabled.com.au>
2016-12-05 12:00:26 +1030

    home scripts

--- website/js/enabled_scripts.js
+++ website/js/enabled_scripts.js
@@ -49,13 +49,13 @@ $(window).scroll(function(){
 
 $.urlParam = function(name){
     var results = new RegExp('[\?&]' + name + '=([^&#]*)').exec(window.location.href);
-    if (results==null){
+    if (results===null){
        return null;
     }
     else{
        return results[1] || 0;
     }
-}
+};
 
 //function slideIsPastTop(slideName)
 //{
