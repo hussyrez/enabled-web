f2a396e379ff49f5b11b32d405f4f4a5cee49dc8
Mohammad Rezai <hussy.rez@gmail.com>
2018-01-31 16:06:21 +1030

    enabled website landing page 1.0

--- site/js/enabled_scripts.js
+++ site/js/enabled_scripts.js
@@ -25,26 +25,119 @@
     closeBttn.on("click", toggleOverlay);
 })();
 $( document ).ready(function() {
-	$(window).scroll(function(){
-		if ($(window).scrollTop() >= 20) {
-			$('#header').addClass('fixed');
-			//$('body.index #wrapper').addClass('reduce');
-			$('#wrapper').addClass('reduce');
-			$('.page-home .fullscreen-video').addClass('hidden');
-		}
-		else {
-		   $('#header').removeClass('fixed');
-		   //$('body.index #wrapper').removeClass('reduce');
-			$('#wrapper').removeClass('reduce');
-			$('.page-home .fullscreen-video').removeClass('hidden');
-		}
+
+        var controller = new ScrollMagic.Controller();
+
+
+        // var pinFirst = new ScrollMagic.Scene({
+        //     triggerElement: '#first',
+        //     triggerHook: 0,
+        //     duration: '500px'
+
+        // })
+        // .setPin('#first', {
+        //     pushFollowers: false
+        // })
+        // .addIndicators({
+        //     name: "div#1 pin"
+        // })
+        // .addTo(controller);
+
+        // var fixNav = new ScrollMagic.Scene({
+        //     triggerElement: '#navbar',
+        //     triggerHook: 0.9
+
+        // })
+        // .setClassToggle('#navbar', 'fixed')
+        // .addIndicators({
+        //     name: "NavFixture"
+        // })
+        // .addTo(controller);
+
+        // var pinSecond = new ScrollMagic.Scene({
+        //     triggerElement: '#second',
+        //     triggerHook: 0.2,
+        //     // duration: '300px'
+
+        // })
+        // .setPin('#first', {
+        //     pushFollowers: false
+        // })
+        // .addTo(controller);
+
+        // var scene = new ScrollMagic.Scene({
+        //     duration: 100,
+        //     triggerElement: '#text',
+        //     triggerHook: 1
+
+        // })
+        // // .setTween("#text", 0.5, {backgroundColor: "green", scale: 2.5, opacity: 1})
+        // .setClassToggle("#text", 'fade-in')
+        // .addIndicators({
+        //     name: "1"
+        // })
+        // .addTo(controller);
+
+       
+//=========================== INDEX PAGE =====================================
+
+
+
+
+        var pinFirst = new ScrollMagic.Scene({
+            triggerElement: '#header',
+            triggerHook: 0,
+            duration: 730
+
+        })
+        .setPin('#header', {
+            pushFollowers: false
+        })
+        .addIndicators({
+            name: "landingPin"
+        })
+        .addTo(controller);
+        $('video').get(0).play();
+
+        var fixedNav = new ScrollMagic.Scene({
+            triggerElement: '#content-holder',
+            // offset: 400,
+            triggerHook: 0.1
+
+        })
+        .setClassToggle('#nav', 'fixed-nav')
+        .addIndicators({
+            name: "NavFixture",
+            offset: 400
+        })
+        .addTo(controller);
+
+        // var addclass = new ScrollMagic.Scene({
+        //     triggerElement: '#content-holder',
+        //     triggerHook: 0.9,
+        // })
+        // .setClassToggle('#header', 'fixed')
+        // .addIndicators({
+        //     name: "Landing Page"
+        // })
+        // .addTo(controller);
+
+        // console.log("Mouse Pos is: " + mouseTopPerc);
+
+		// if ($(window).scrollTop() >= 20) {
+		// 	$('#header').addClass('fixed');
+		// 	//$('body.index #wrapper').addClass('reduce');
+		// 	$('#wrapper').addClass('reduce');
+		// 	$('.page-home .fullscreen-video').addClass('hidden');
+		// }
+		// else {
+		//    $('#header').removeClass('fixed');
+		//    //$('body.index #wrapper').removeClass('reduce');
+		// 	$('#wrapper').removeClass('reduce');
+		// 	$('.page-home .fullscreen-video').removeClass('hidden');
+		// }
 		
-		//if( slideIsPastTop(".content-slider-holder") )
-		//{
-			//console.log("Starting playback");
-			//$('.content-slider').slick('slickPlay');
-		//}
-	});
+
 });
 
 $.urlParam = function(name){
